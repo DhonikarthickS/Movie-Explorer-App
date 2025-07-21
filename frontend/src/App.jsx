9c1f987bde3866b18ddb732d250207efd03ad0cf
DhonikarthickS <dhonikarthicksalem@gmail.com>
2025-07-21 22:57:51 +0530

    Update: Modified components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,18 @@
-import './css/App.css';
+import "./css/App.css";
 import Home from "./pages/Home";
-import Favorites from './pages/Favorites';
-import { Routes, Route } from 'react-router-dom';
-import NavBar from './components/NavBar';
-import { MovieProvider } from './context/MovieContext';
+import Favorites from "./pages/Favorites";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import { MovieProvider } from "./context/MovieContext";
 
 function App() {
   return (
     <MovieProvider>
       <NavBar />
-      <main className='main-content'>
+      <main className="main-content">
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/favorites' element={<Favorites />} />
+          <Route path="/Movie-Explorer-App" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
         </Routes>
       </main>
     </MovieProvider>
